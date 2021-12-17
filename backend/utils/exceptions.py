06260ae32f25f8cef7a535bce1bcdf9e1e6797d3
Oren Zhang <zhxdida@outlook.com>
2021-12-17 12:39:37 +0800

    minor: add translation

--- backend/utils/exceptions.py
+++ backend/utils/exceptions.py
@@ -1,4 +1,5 @@
 from django.http import Http404
+from django.utils.translation import gettext_lazy as _
 from rest_framework import exceptions, status
 from rest_framework.exceptions import APIException, Throttled, ValidationError
 from rest_framework.response import Response
@@ -55,76 +56,76 @@ def exception_handler(exc, context):
 class ServerError(APIException):
     status_code = status.HTTP_500_INTERNAL_SERVER_ERROR
     default_code = 500500
-    default_detail = "服务器异常，请联系管理员"
+    default_detail = _("服务器异常，请联系管理员")
 
 
 class LoginRequired(APIException):
     status_code = status.HTTP_401_UNAUTHORIZED
     default_code = 500401
-    default_detail = "未登录用户"
+    default_detail = _("未登录用户")
 
 
 class LoginFailed(APIException):
     status_code = status.HTTP_401_UNAUTHORIZED
     default_code = 500401
-    default_detail = "登录失败"
+    default_detail = _("登录失败")
 
 
 class VerifyCodeFailed(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_code = 500400
-    default_detail = "验证码有误"
+    default_detail = _("验证码有误")
 
 
 class UserNotExist(APIException):
     status_code = status.HTTP_404_NOT_FOUND
     default_code = 500404
-    default_detail = "用户不存在"
+    default_detail = _("用户不存在")
 
 
 class UsernameExist(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_code = 500400
-    default_detail = "用户名已存在"
+    default_detail = _("用户名已存在")
 
 
 class PhoneNumberExist(UsernameExist):
-    default_detail = "手机号已被注册"
+    default_detail = _("手机号已被注册")
 
 
 class PermissionDenied(APIException):
     status_code = status.HTTP_403_FORBIDDEN
     default_code = 500403
-    default_detail = "未经授权访问"
+    default_detail = _("未经授权访问")
 
 
 class ParamsNotFound(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_code = 500400
-    default_detail = "参数缺失"
+    default_detail = _("参数缺失")
 
 
 class ThrottledError(Throttled):
     status_code = status.HTTP_429_TOO_MANY_REQUESTS
     default_code = 500429
-    default_detail = "请求次数受限"
-    extra_detail_singular = "请在{wait}秒后再试"
+    default_detail = _("请求次数受限")
+    extra_detail_singular = _("请在{wait}秒后再试")
     extra_detail_plural = extra_detail_singular
 
 
 class SMSSendFailed(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_code = 500400
-    default_detail = "短信发送失败，请稍后再试"
+    default_detail = _("短信发送失败，请稍后再试")
 
 
 class OperationError(APIException):
     status_code = status.HTTP_400_BAD_REQUEST
     default_code = 500400
-    default_detail = "异常操作"
+    default_detail = _("异常操作")
 
 
 class Error404(APIException):
     status_code = status.HTTP_404_NOT_FOUND
     default_code = 500404
-    default_detail = "资源不存在"
+    default_detail = _("资源不存在")
