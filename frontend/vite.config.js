4602b111a33fdd96e04f939ee75200016e4ca07d
Oren Zhang <zhxdida@outlook.com>
2021-12-18 13:00:19 +0800

    minor: change component import mode

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,15 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
-import Components from 'unplugin-vue-components/vite'
-import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
 import prismjs from 'vite-plugin-prismjs'
 
 export default defineConfig({
     plugins: [
         vue(),
-        Components({
-            resolvers: [ElementPlusResolver()]
-        }),
         prismjs({
             languages: [
                 'bash',
