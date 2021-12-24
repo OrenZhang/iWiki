67175a07e0b1c61331f7b95aec100a0833e80861
Oren Zhang <zhxdida@outlook.com>
2021-12-24 16:03:06 +0800

    minor: 更换用户认证模式

--- backend/entry/urls.py
+++ backend/entry/urls.py
@@ -13,12 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
     path("favicon.ico", RedirectView.as_view(url="https://wiki.incv.net/favicon.ico")),
+    path("admin/login/", RedirectView.as_view(url=f"{settings.SIMPLEUI_INDEX}/admin")),
+    path("admin/logout/", RedirectView.as_view(url=f"{settings.SIMPLEUI_INDEX}/admin")),
     path("admin/", admin.site.urls),
     path("i18n/", include("modules.i18n.urls")),
     path("account/", include("modules.account.urls")),
