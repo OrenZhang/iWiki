286fe3d5d9fd41317b70946a33d4bbad5e654e73
Oren Zhang <zhxdida@outlook.com>
2022-01-11 13:05:31 +0800

    bugfix: 申请错误

--- backend/modules/repo/permissions.py
+++ backend/modules/repo/permissions.py
@@ -1,4 +1,3 @@
-from django.db.models import Q
 from rest_framework.permissions import BasePermission
 
 from constents import UserTypeChoices
@@ -22,22 +21,3 @@ class RepoAdminPermission(BasePermission):
         if request.method != "DELETE" and repo_user.u_type == UserTypeChoices.ADMIN:
             return True
         raise PermissionDenied()
-
-
-class RepoCommonPermission(BasePermission):
-    def has_object_permission(self, request, view, obj: Repo):
-        # 超级管理员拥有全部权限
-        if request.user.is_superuser:
-            return True
-        # 获取单个库的信息需要库成员身份
-        if view.action == "retrieve":
-            try:
-                RepoUser.objects.get(
-                    Q(repo_id=obj.id, uid=request.user)
-                    & ~Q(u_type=UserTypeChoices.VISITOR)
-                )
-                return True
-            except RepoUser.DoesNotExist:
-                raise PermissionDenied()
-        # 其余接口为公开接口
-        return True
