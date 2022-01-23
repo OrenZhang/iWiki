74a4321ac36dbb2f63acf38c1998067a48f546cd
Oren Zhang <zhxdida@outlook.com>
2022-01-23 10:32:37 +0800

    bugfix: 评论版本不生效 (#202)

--- backend/modules/doc/views/comment.py
+++ backend/modules/doc/views/comment.py
@@ -58,7 +58,14 @@ class CommentCommonView(
         return Response(CommentListSerializer(instance).data)
 
     def update(self, request, *args, **kwargs):
-        return super().update(request, *args, **kwargs)
+        partial = kwargs.pop("partial", False)
+        instance = self.get_object()
+        serializer = self.get_serializer(instance, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        with transaction.atomic():
+            self.perform_update(serializer)
+            CommentVersion.objects.create(**CommentCommonSerializer(instance).data)
+        return Response(serializer.data)
 
     def destroy(self, request, *args, **kwargs):
         instance = self.get_object()
