4ae03a8f383dbf76bf829e4d55c3cd728cf513f2
Oren Zhang <zhxdida@outlook.com>
2022-03-16 12:27:48 +0800

    minor: 优化 Celery 日志 (#288)

--- backend/bin/run.sh
+++ backend/bin/run.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
 python manage.py migrate
-nohup celery -A modules.cel worker -l INFO > /dev/null 2>&1 &
-nohup celery -A modules.cel beat -l INFO > /dev/null 2>&1 &
+nohup celery -A modules.cel worker -l INFO -f /usr/src/iwiki-backend/celery-logs/worker.log > /dev/null 2>&1 &
+nohup celery -A modules.cel beat -l INFO -f /usr/src/iwiki-backend/celery-logs/beat.log > /dev/null 2>&1 &
 uwsgi --ini /usr/src/iwiki-backend/uwsgi.ini --processes $UWSGI_PROCESSES --threads $UWSGI_THREADS -w wsgi.wsgi:application > /dev/null 2>&1
