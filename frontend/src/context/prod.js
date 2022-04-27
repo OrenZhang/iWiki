16f763910873355fb399054ddd74c2347f90c4fb
orenzhang <zhxdida@outlook.com>
2022-04-27 19:41:25 +0800

    feature: 收藏文章

--- frontend/src/context/prod.js
+++ frontend/src/context/prod.js
@@ -1,4 +1,4 @@
 export const prodConfig = {
-    siteUrl: 'https://wiki.oren.ink/',
-    backEndUrl: 'https://api.wiki.oren.ink'
+    siteUrl: 'https://wiki.incv.net/',
+    backEndUrl: 'https://api.wiki.incv.net'
 }
