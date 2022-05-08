96cfba8a31000665417446cf84008be44557f966
orenzhang <zhxdida@outlook.com>
2022-05-08 17:46:22 +0800

    bugfix: uwsgi环境不上报监测日志

--- backend/bin/run.sh
+++ backend/bin/run.sh
@@ -3,4 +3,12 @@
 python manage.py migrate
 nohup celery -A modules.cel worker -l INFO -f /usr/src/iwiki-backend/celery-logs/worker.log > /dev/null 2>&1 &
 nohup celery -A modules.cel beat -l INFO -f /usr/src/iwiki-backend/celery-logs/beat.log > /dev/null 2>&1 &
-uwsgi --ini /usr/src/iwiki-backend/uwsgi.ini --processes $UWSGI_PROCESSES --threads $UWSGI_THREADS -w wsgi.wsgi:application > /dev/null 2>&1
+uwsgi \
+--http :8014 \
+--processes $UWSGI_PROCESSES \
+--threads $UWSGI_THREADS \
+--module entry.wsgi \
+--logto logs/uwsgi.log \
+--static-map /static=/usr/src/iwiki-backend/static \
+--log-maxsize 10000000
+> /dev/null 2>&1
--- backend/entry/settings.py
+++ backend/entry/settings.py
@@ -29,6 +29,7 @@ from pathlib import Path
 from django.utils.translation import gettext_lazy as _
 from opentelemetry import trace
 from opentelemetry.exporter.jaeger.thrift import JaegerExporter
+from opentelemetry.instrumentation.django import DjangoInstrumentor
 from opentelemetry.sdk.resources import Resource, SERVICE_NAME
 from opentelemetry.sdk.trace import TracerProvider
 from opentelemetry.sdk.trace.export import BatchSpanProcessor
@@ -335,13 +336,10 @@ SIMPLEUI_CONFIG = {"system_keep": False}
 DEFAULT_REPO_NAME = getenv_or_raise("DEFAULT_REPO_NAME")
 
 # opentracing
-IS_USING_JAEGER_EXPORT = (
-    True if os.getenv("IS_USING_JAEGER_EXPORT", "False") == "True" else False
-)
-if IS_USING_JAEGER_EXPORT:
-    JAEGER_HOST = os.getenv("JAEGER_HOST", "localhost")
-    TRACE_PROVIDER = TracerProvider(resource=Resource.create({SERVICE_NAME: APP_CODE}))
-    JAEGER_EXPORTER = JaegerExporter(agent_host_name=JAEGER_HOST, agent_port=6831)
-    SPAN_PROCESSOR = BatchSpanProcessor(JAEGER_EXPORTER)
-    TRACE_PROVIDER.add_span_processor(SPAN_PROCESSOR)
-    trace.set_tracer_provider(TRACE_PROVIDER)
+JAEGER_HOST = os.getenv("JAEGER_HOST", "localhost")
+TRACE_PROVIDER = TracerProvider(resource=Resource.create({SERVICE_NAME: APP_CODE}))
+JAEGER_EXPORTER = JaegerExporter(agent_host_name=JAEGER_HOST, agent_port=6831)
+SPAN_PROCESSOR = BatchSpanProcessor(JAEGER_EXPORTER)
+TRACE_PROVIDER.add_span_processor(SPAN_PROCESSOR)
+trace.set_tracer_provider(TRACE_PROVIDER)
+DjangoInstrumentor().instrument()
