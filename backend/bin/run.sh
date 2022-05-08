e658aea73ef0d2ebb3ab453ee996786f0e9c39a4
orenzhang <zhxdida@outlook.com>
2022-05-08 18:32:25 +0800

    bugfix: uwsgi环境不上报监测日志

--- backend/bin/run.sh
+++ backend/bin/run.sh
@@ -5,10 +5,9 @@ nohup celery -A modules.cel worker -l INFO -f /usr/src/iwiki-backend/celery-logs
 nohup celery -A modules.cel beat -l INFO -f /usr/src/iwiki-backend/celery-logs/beat.log > /dev/null 2>&1 &
 uwsgi \
 --http :8014 \
---processes $UWSGI_PROCESSES \
+--processes 1 \
 --threads $UWSGI_THREADS \
 --module entry.wsgi \
 --logto /usr/src/iwiki-backend/logs/uwsgi.log \
 --static-map /static=/usr/src/iwiki-backend/static \
---log-maxsize 10000000
-> /dev/null 2>&1
+--log-maxsize 10000000 > /dev/null 2>&1
