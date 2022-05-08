da1cf8ef7c075f8f27df9a1f39f66b698b801d0b
Oren Zhang <zhxdida@outlook.com>
2022-05-08 20:58:49 +0800

    minor: 前端发布脚本 (#387)

---
+++ frontend/build.sh
@@ -0,0 +1,14 @@
+DOMAIN=$1
+
+cp src/context/prod.js src/context/prod.js.bak
+
+if [ $DOMAIN ]; then
+  echo -e "\033[36mDomain is $DOMAIN\033[0m"
+  sed -i '' 's/wiki.incv.net/'"${DOMAIN}"'/g' src/context/prod.js
+else
+  echo -e "\033[36mDomain is wiki.incv.net\033[0m"
+fi
+
+yarn build
+
+mv src/context/prod.js.bak src/context/prod.js
